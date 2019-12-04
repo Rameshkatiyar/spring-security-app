21aff967f3ca07ec0c34524049ff6340dcf78a0b
ramesh75 <ramesh.katiyar@evivehealth.com>
2019-12-04 18:33:06 +0530

    Created one simple web app.

--- src/main/java/com/tech/controller/HomeController.java
+++ src/main/java/com/tech/controller/HomeController.java
@@ -1,4 +1,13 @@
 package com.tech.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
 }
